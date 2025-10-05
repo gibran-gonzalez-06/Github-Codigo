3cb6c8e2a496f9d6c8064a90e7b6291323a7a499
gibran-gonzalez-06 <gibrangonzalez804@gmail.com>
2025-10-05 02:13:53 -0600

    Project Star

--- codigoMaxMin.py
+++ codigoMaxMin.py
@@ -1,3 +1,11 @@
+#Error de orden de Ejecución:El bloque correcto estara al final del archivo
+#Estas son constantes de configutación que se pueden usar en metodos numericos u optimización
+MAX_ITERS = 100
+TOL = 1e - 8
+M = 1e6
+#Programa para solucionar problemas de optimización con el metodo simplex
+
+
 # Importamos las librerias necesarias
 import numpy as np
 from scipy.optimize import linprog
@@ -5,10 +13,21 @@ from scipy.optimize import linprog
 # Funcion para solicitar los datos al usuario
 def leerdatos():
     print("=== Metodo Simplex ===")
-    tipo= input("¿El problema es de Maximización o Minimización?")
+    tipo= input("¿Desea Maximizar o Minimizar? solo coloca: (Max/Min):").strip().lower()
+    while tipo not in["Max", "Min"]:
+       print("Entrada no valida intente de nuevo")
+       tipo= input("¿Desea Maximizar o Minimizar? solo coloca: (Max/Min):").strip().lower()
 
-    #Numero de Variables
-    n_vars = int(input("¿Cuantas variables tiene la funcion objetivo?"))
+    #Solicitamos el Numero de Variables
+    n = int(input("Ingresa el numero de variables:"))
+
+    #Solicitamos los coeficientes de la función objetivo.
+    print("Ingresa los coeficientes de la funcion objetivo:")
+    c = [] #Coeficientes que agreguemos
+    for i in range(n_vars):
+     coef = float(input(f"coeficiente de x{i+1}:"))
+     c.append(coef)
+
 
     # Leemos los coeficientes de la función colectivo
     print("\nIntroduce los coeficientes de la función objetivo:")
@@ -19,7 +38,7 @@ def leerdatos():
 
       #Si es maximización cambiamos el signo de los coeficientes
       #Porque Lingpro resuelve solo problemas de Minimización
-      if tipo == "Minimización":
+      if tipo == "Maximización":
          c = [-x for x in c]
          # Leemos el numero de restricciones
          n_restricciones = int(input("\n¿Cuantas Restricciones hay?"))
@@ -37,7 +56,7 @@ def leerdatos():
                fila.append(coef)
             signo = input("Signo de la restricción (<=,>=,=):").Strip()
             valor = float(input("Valor del lado derecho: "))
-            #Ajustamos el signo de la igualdad
+            #Para igualdad agregamos dos restricciones
             if signo== "<=":
                A.append(fila)
                B.append(valor)
